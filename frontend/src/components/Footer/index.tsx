7faae0bbf78b57353e29d63042025056326b97df
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2021-09-11 16:00:25 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/digaomestre" target="_blank" rel="noreferrer">Rodrigo Tomaz</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="linkedin.com/in/rodrigo-tomaz-53529912a" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
